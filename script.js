1d235a9c6691ee6b857c2b91746c4563fabfdfd6
¨alejandrapitta¨ <alejandrapitta02@gmail.com>
2024-11-15 13:55:59 +0100

    java

---
+++ script.js
@@ -0,0 +1,30 @@
+// alert("hola mundo");
+// alert(1+9+41);
+
+// alert () muestra una ventana emergente 
+
+let saludo = "holaaa";
+
+let despedida;
+despedida = "Adios";
+
+saludo = despedida;
+
+
+let nombreUsuario = "Alejandra";
+
+let edad = 16;
+edad = 17;
+edad+1;  //  puedo sumar a la variable porque guarda un valor numerico 
+edad++;  //  suma una 
+edad--;  //    resta una 
+
+// console ()muestra lo que pongamos dentro de los parentesis dentro de la consola 
+console.log(edad);
+
+alert(saludo+" "+nombreUsuario); //  puedo concatenar strings usando +
+
+// constantes
+const SALUDO_USUARIO = saludo+nombreUsuario;
+
+
