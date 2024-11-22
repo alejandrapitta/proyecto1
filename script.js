5958a8f6697218d6e52dea85296c7d82ed178241
alejandrapitta <alejandrapitta02@gmail.com>
2024-11-22 20:29:18 +0100

    funciones barra

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let nombreUsuario = "Alejandra";
 
 let edad = 16;
 edad = 17;
-edad = edad+1; 
+edad = edad + 1;
 edad += 1; //  puedo sumar a la variable porque guarda un valor numerico 
 edad++;  //  suma una 
 edad--;  //  resta una 
@@ -22,10 +22,10 @@ edad--;  //  resta una
 // console () muestra lo que pongamos dentro de los parentesis dentro de la consola 
 console.log(edad);
 
-alert(saludo+" "+nombreUsuario); //  puedo concatenar strings usando +
+alert(saludo + " " + nombreUsuario); //  puedo concatenar strings usando +
 
 // const: constantes(para distinguirla la escribo ne mayuscula,ya despues de escrita no se puede editar)
-const SALUDO_USUARIO = saludo+nombreUsuario;
+const SALUDO_USUARIO = saludo + nombreUsuario;
 
 // Datos en comillas, sera un dato de texto STRING 
 // boolean solo da la opcion e false y true
@@ -42,7 +42,7 @@ console.log(5 === "5");// extrictamente igual que
 console.log(5 != 7);// no es igual
 
 // Puedo comparar texto
-console.log("a">"b"); 
+console.log("a" > "b");
 console.log("cortazar" > "Navarro");
 
 
@@ -50,12 +50,12 @@ let premium = true;
 
 if (premium) {
     console.log("tienes cuenta premium");
-    alert ("Bienvenido" + nombreUsuario)
+    alert("Bienvenido" + nombreUsuario)
 }
 
 // si no se cumple la condicion if, la opcion else no se va a ejecutar 
 
-else if (edad>=18){
+else if (edad >= 18) {
     console.log("Puedes crear cuenta");
 }
 else {
@@ -65,16 +65,16 @@ else {
 // Para interactuar con html dom
 // SELECTORES
 //        recojo el elemento    // que hago//
-document.getElementById("texto").innerHTML = "Holaaa "+nombreUsuario;
+document.getElementById("texto").innerHTML = "Holaaa " + nombreUsuario;
 
 document.getElementsByClassName("hola").innerHTML = "<p> Hola otra vez </p>";
 
 document.getElementsByClassName("articulo").innerHTML = "gasdagd0";
 
- 
-document.getElementById("texto").innerHTML = "holaaaa"+nombreUsuario;
-    document.getElementById("caja").innerHTML ="<p>Hasta luego</p>"
-    //con .innerhtml voy agregarle texo directamrnte desde java a html
+
+document.getElementById("texto").innerHTML = "holaaaa" + nombreUsuario;
+document.getElementById("caja").innerHTML = "<p>Hasta luego</p>"
+//con .innerhtml voy agregarle texo directamrnte desde java a html
 
 // si quiero que me seleccione el primer elemento
 //  document.querySelector("#texto").innerHTML = "123456"
@@ -86,35 +86,47 @@ texto.innerHTML = "He guardado mi selector en una variable";
 
 
 
-    //FUNCIONES
+//FUNCIONES
 
-    function saludar(){
+// function ingresar(){
+// alert("Bienvenido Usuario")
+// }
 
-        alert("Bienvenido")
-    }
-    saludar();
+function ingresar() {
+    let inputUsuario = document.querySelector("#correo-usuario").value
+    alert("Hola de nuevo " + inputUsuario);
+}
+// -----------------------------------
 
-    function modoNoche(){
-   document.body.style.backgroundColor = "black";
 
-    }
-    function modoDia(){
-       document.body.style.background = "white";
-    }
+
+function hola() {
+
+    alert("Bienvenido")
+}
+hola();
+
+function modoNoche() {
+    document.body.style.backgroundColor = "black";
+
+}
+function modoDia() {
+    document.body.style.background = "white";
+}
 // --------estrellas cayendo------------------
-    function createStar() {
-        const Star = document.createElement('div');
-        Star.classList.add('star');
-        Star.style.left = Math.random() * 100 + 'vw';
-        Star.style.animationDuration = Math.random() * 6 + 9 + 's';
-        document.body.appendChild(Star);
-        setTimeout(() => {
+function createStar() {
+    const Star = document.createElement('div');
+    Star.classList.add('star');
+    Star.style.left = Math.random() * 100 + 'vw';
+    Star.style.animationDuration = Math.random() * 6 + 9 + 's';
+    document.body.appendChild(Star);
+    setTimeout(() => {
         Star.remove();
-        }, 5000);
-    }
-    setInterval(createStar, 300);
+    }, 5000);
+}
+setInterval(createStar, 300);
 
 // -------------------------------------------
-    
+
 
 
